a4801a7665a0ffe3ef8fb8f3bcdef2e6284f8a71
igor <igor.pravdin163@gmail.com>
2023-06-02 15:22:49 +0400

    2 уровень. admin, models, settings, views

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
 
 # Register your models here.
--- blog/website/models.py
+++ blog/website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    '''Данные о записи'''
+    title = models.CharField('Заголовок записи', max_length=50)
+    descriptions = models.TextField('Описание', max_length=1000)
+    author = models.CharField('Имя автора', max_length=100)
+    date = models.DateField('Дата публикации')
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+
+
