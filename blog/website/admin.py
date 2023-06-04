f4149a78530514372f326aed0a9dc646080b325f
Pravdin763 <igor.pravdin163@gmail.com>
2023-06-04 15:20:32 +0400

    уровень 5 добавил форму комментариев

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comments
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date')
     list_filter = ('date',)
+
+@admin.register(Comments)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post')
 
 
 # Register your models here.
--- blog/website/models.py
+++ blog/website/models.py
@@ -16,3 +16,16 @@ class Post(models.Model):
         verbose_name_plural = 'Записи'
 
 
+class Comments(models.Model):
+    '''Создание комментариев'''
+    email = models.EmailField()
+    name = models.CharField('Имя', max_length=50)
+    text_comment = models.TextField('Текст комментария', max_length=200)
+    post = models.ForeignKey(Post, verbose_name='Публикация', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}, {self.email}, {self.post}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
